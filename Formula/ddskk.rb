192fa1e1cc4832c1cfd86ba5204108d52d3654ba
Marica Odagaki <ento.entotto@gmail.com>
2011-07-23 20:34:09 +0900

    New formula: ddskk
    Taken from
    https://gist.github.com/515462/1ffd87815f352c6f888a5052a9e41238253f7058#file_ddskk.rb
    + modifications

---
+++ Formula/ddskk.rb
@@ -0,0 +1,61 @@
+require 'formula'
+
+class Ddskk <Formula
+  url 'http://openlab.ring.gr.jp/skk/maintrunk/ddskk-14.3.tar.gz'
+  homepage 'http://openlab.ring.gr.jp/skk/index-j.html'
+  md5 'af8aab148acc9c3861b9f0faba6fe9c8'
+
+  depends_on 'apel'
+
+  def caveats
+    s = ""
+    s += <<-EOS.undent
+    Add the following to your .emacs file:
+
+    (add-to-list 'load-path "#{@install_dir}")
+    (require 'skk-autoloads)
+
+    EOS
+
+    if !ARGV.include? "--skk-set-jisyo"
+      s += <<-EOS.undent
+      You choosed not to set skk-large-jisyo automatically.
+      Which means you need to connect to a skkserv like below:
+
+      (setq skk-server-host "localhost")
+      (setq skk-server-portnum 1178)
+      (setq skk-share-private-jisyo t)
+      (setq skk-jisyo-code 'utf-8-unix)
+      EOS
+    end
+  end
+  
+  def options
+    [
+      ["--skk-set-jisyo", "Automatically set skk-large-jisyo"],
+    ]
+  end
+
+  def install
+    @site_lisp_path = "share/emacs/site-lisp"
+    @install_dir = "#{HOMEBREW_PREFIX}#{@site_lisp_path}/skk"
+
+    open("SKK-CFG", 'a'){|f|
+      f.puts(<<DATA
+(setq APEL_DIR "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/apel")
+(setq EMU_DIR "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/emu")
+(setq SKK_DATADIR "#{HOMEBREW_PREFIX}/share/skk")
+(setq SKK_INFODIR "#{HOMEBREW_PREFIX}/info")
+(setq SKK_LISPDIR "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/skk")
+DATA
+             )
+      f.puts(<<DATA
+(setq SKK_SET_JISYO t)
+DATA
+             ) if ARGV.include? "--skk-set-jisyo"
+    }
+
+    system "make"
+    system "make install"
+  end
+end
